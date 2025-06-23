0c6b338179e1a5c27e2c60b12ebd307dfa868141
lakshuki63 <lakshukihatwar@gmail.com>
2025-06-23 15:38:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Landing Page/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Landing Page/Home";
 
 // import Register from './components/Login/Register';
-import NGORegister from './components/Login/NGORegister';
-import NGOLogin from './components/Login/NGOLogin';
-import UserRegister from './components/Login/UserRegister';
-import UserLogin from './components/Login/UserLogin';
+import NGORegister from "./components/Login/NGORegister";
+import NGOLogin from "./components/Login/NGOLogin";
+import UserRegister from "./components/Login/UserRegister";
+import UserLogin from "./components/Login/UserLogin";
 
-import Navbar from './components/Landing Page/Navbar';
-import AboutUs from './components/Landing Page/AboutUs';
-import Dashboard from './components/Dashboard/Dashboard';
-import LinkedInHome from './components/LinkedInHome';
-
+import Navbar from "./components/Landing Page/Navbar";
+import AboutUs from "./components/Landing Page/AboutUs";
+import Dashboard from "./components/Dashboard/Dashboard";
+import LinkedInHome from "./components/link/LinkedInHome";
 
 function App() {
   return (
@@ -26,7 +25,7 @@ function App() {
         <Route path="/NGOLogin" element={<NGOLogin />} />
         <Route path="/Userregister" element={<UserRegister />} />
         <Route path="/UserLogin" element={<UserLogin />} />
-        
+
         <Route path="/about" element={<AboutUs />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/LinkedInHome" element={<LinkedInHome />} />
